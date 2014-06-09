1d8d759a616f5a92ab15c6b45cc6fa143e1d7878
Sam Eubank <sameubank+zerious@gmail.com>
2014-06-08 17:27:18 -0700

    Updated Jymin

--- scripts/d6-jymin.js
+++ scripts/d6-jymin.js
@@ -5,6 +5,12 @@
  * Otherwise use ../d6-client.js which includes required Jymin functions.
  */
 
+var d6Load = function (url) {
+  if (window.d6) {
+    d6._LOAD(url);
+  }
+};
+
 (function () {
 
   /**
@@ -17,7 +23,7 @@
     }
   };
 
-  var views = d6.views = {};
+  var views = d6._VIEWS = {};
 
   var cache = {};
 
@@ -99,30 +105,49 @@
   /**
    * Load a URL via GET request.
    */
-  var loadUrl = function (url, callback) {
+  var loadUrl = d6._LOAD = function (url, callback) {
+
+    // Set all spinners in the page to their loading state.
+    all('._SPINNER', function (spinner) {
+      addClass(spinner, '_LOADING');
+    });
+
     // A resource is either a cached response, a callback queue, or nothing.
     var resource = cache[url];
+
+    // If there's no resource, start the JSON request.
     if (!resource) {
       getD6Json(url, callback);
     }
+    // If the "resource" is a callback queue, then pushing means listening.
     else if (isArray(resource)) {
-      resource.push(callback);
+      push(resource, callback);
     }
+    // If the resource exists and isn't an array, render it.
     else {
       resource.request.url = url;
       renderResponse(resource);
     }
   };
 
+  // Request JSON, indicating with a URL param that D6 is requesting it.
   var getD6Json = function (url, callback) {
-    url += (containsString(url, '?') ? '&' : '?') + 'd6=on';
+    url += (contains(url, '?') ? '&' : '?') + 'd6=on';
     getJson(url, callback);
   };
 
+  // Render a template with the given context, and display the resulting HTML.
   var renderResponse = function (context) {
     var err = context._ERROR;
     var view = views[context.view];
     var html;
+
+    // Reset any spinners.
+    all('._SPINNER', function (spinner) {
+      removeClass(spinner, '_LOADING');
+    });
+
+    // If there's an error, render the error text.
     if (err) {
       html = context._TEXT;
       //+env:dev
@@ -130,10 +155,14 @@
       //-env:dev
       writeHtml(html);
     }
+
+    // If the context refers to a view that we have, render it.
     else if (view) {
       html = view.call(views, context);
       writeHtml(html);
     }
+
+    // If we can't find a corresponding view, navigate the old-fashioned way.
     else {
       //+env:dev
         error('View not found: "' + context.view + '"');
